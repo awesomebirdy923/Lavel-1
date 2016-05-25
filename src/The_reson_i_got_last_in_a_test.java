9bac6ad122a16a6f1a0bf9b8851af497f9e06f9a
TJHL <league@WTS5.attlocal.net>
2016-05-25 16:18:54 -0700

    hi
    hi

---
+++ src/The_reson_i_got_last_in_a_test.java
@@ -0,0 +1,30 @@
+
+public class The_reson_i_got_last_in_a_test {
+
+	String reason;
+	String why;
+
+	public The_reson_i_got_last_in_a_test(String reason, String why) {
+		this.reason = reason;
+		this.why = why;
+	}
+
+	public String getWhy() {
+		return why;
+	}
+
+	public void setWhy(String why) {
+		this.why = why;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	
+	
+	
+}
---
+++ src/runner.java
@@ -0,0 +1,11 @@
+
+public class runner {
+	public static void main(String[] args) {
+		The_reson_i_got_last_in_a_test nonsense = new The_reson_i_got_last_in_a_test("there is a reason", "i got last");
+		The_reson_i_got_last_in_a_test huuhh = new The_reson_i_got_last_in_a_test("Because", " why not");
+		System.out.println(nonsense.getReason() + " that I got the least greatest score because " + nonsense.getWhy());
+		System.out.println(huuhh.getReason() + " you did and  " + huuhh.getWhy());
+		The_reson_i_got_last_in_a_test wow = new The_reson_i_got_last_in_a_test("i got the worst score",
+				"i didn't get a better score than anyone else");
+	}
+}

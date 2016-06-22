5768291db63816027c8aab46dca6456bbbdb5c7f
TJHL <league@WTS5.attlocal.net>
2016-06-22 16:36:13 -0700

    hi
    hi

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+public void assertEquals(int expected, int resalt){
+	if(expected == resalt){
+		System.out.println("Yay!");
+	}else{
+		System.err.println("NOOOOOOO");
+	}
+}
+}
